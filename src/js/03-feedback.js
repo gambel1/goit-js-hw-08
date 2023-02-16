b65deb943ce780ecdae305940487752b2ab47d46
Алексей <electrifyx@gmail.com>
2023-02-16 17:59:12 +0200

    edit homework

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,51 +4,31 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const formEl = document.querySelector('.feedback-form');
 const inputEl = document.querySelector('input');
-
-const formData = {};
-
-populateHandleInput();
+// const textareaEl = document.querySelector('.feedback-form textarea');
 
 formEl.addEventListener('submit', onFormSubmit);
 inputEl.addEventListener('input', throttle(onInputEl, 500));
 
+const formData = {};
+
 function onInputEl(event) {
   formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
-const savedMessage = localStorage.getItem(
-  STORAGE_KEY,
-  JSON.stringify(formData)
-);
-// const parsedMessage = JSON.parse(savedMessage);
 
 function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 }
 
-function onInputEl(event) {
-  const message = event.target.value;
-  localStorage.setItem(STORAGE_KEY, message);
-}
+localStorageHandleData();
 
-function load(key) {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.log(error.name);
-  }
-}
-
-function populateHandleInput() {
-  if (load(STORAGE_KEY)) {
-    const outputForm = load(STORAGE_KEY);
-    const formKeys = Object.keys(outputForm);
-
-    formKeys.map(element => {
-      document.querySelector(`[name='${element}]`).value = outputForm[element];
-    });
+function localStorageHandleData() {
+  const handleData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (handleData) {
+    // inputEl.value = handleData.inputEl;
+    // textareaEl.value = handleData.textareaEl;
   }
 }
